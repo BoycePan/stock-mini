f51bc645bf32d92e935f0716eabc65e10b5d0bda
lilaiyun <laiyun_li@163.com>
2026-08-23 17:01:49 +0800

    fix: refine authInterceptor path mappings in WebConfig
    - Re-enable authInterceptor for `/api/v1/user/**` and `/api/v1/**`
    - Exclude `/api/v1/auth/**` from interception

--- backend-java/src/main/java/com/guyu/stock/config/WebConfig.java
+++ backend-java/src/main/java/com/guyu/stock/config/WebConfig.java
@@ -20,8 +20,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/api/v1/user/**");
 
         registry.addInterceptor(mgrAuthInterceptor)
                 .addPathPatterns("/api/mgr/**")
@@ -32,9 +30,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/v1/track/**");
 
 
-//        registry.addInterceptor(authInterceptor)
-//                .addPathPatterns("/api/v1/user/**")
-//                .addPathPatterns("/api/v1/**")
-//                .excludePathPatterns("/api/v1/auth/**");
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/v1/user/**")
+                .addPathPatterns("/api/v1/**")
+                .excludePathPatterns("/api/v1/auth/**");
     }
 }
